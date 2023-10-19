0edcb259f734365f204f7dd6c6d1eaac41aa3b9a
allperdogan <47662425+allperdogan@users.noreply.github.com>
2023-10-19 23:45:00 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<List<Product>> GettAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IResult Add(Product product);
+        IDataResult<Product> GetById(int id);
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,52 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //IoC
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success)
+            {
+                foreach (var product in productManager.GetProductDetails().Data)
+                {
+                    Console.WriteLine(product.ProductName + " " + product.CategoryName);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+        }
+    }
+}
